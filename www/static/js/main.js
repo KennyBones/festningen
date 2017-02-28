6309c7a007742f3523cba057d160cb0e4e3c3886
KennyBones <raziel014@gmail.com>
2017-02-28 18:21:36 +0100

    Masse greier

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -14,6 +14,7 @@ if (typeof FastClick === 'function') { FastClick.attach(document.body); }
         $window       = $m(window),
         $html         = $m(document.documentElement),
         $document	  = $m(document),
+        map,
         $selskap_sticky_block = $m('.selskap .sticky-block'),
         $restaurant_sticky_block = $('.restaurant .sticky-block'),
         desktop,
@@ -74,6 +75,181 @@ if (typeof FastClick === 'function') { FastClick.attach(document.body); }
 
 
 	$html.addClass('lastet');
+
+  // Sleng på Google map
+  function initMap() {
+    //'use strict';
+
+        var styles = [{
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "hue": "#ff4400"
+              },
+              {
+                  "saturation": -68
+              },
+              {
+                  "lightness": -4
+              },
+              {
+                  "gamma": 0.72
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "labels.icon"
+      },
+      {
+          "featureType": "landscape.man_made",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "hue": "#0077ff"
+              },
+              {
+                  "gamma": 3.1
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "stylers": [
+              {
+                  "hue": "#00ccff"
+              },
+              {
+                  "gamma": 0.44
+              },
+              {
+                  "saturation": -63
+              }
+          ]
+      },
+      {
+          "featureType": "poi.park",
+          "stylers": [
+              {
+                  "hue": "#44ff00"
+              },
+              {
+                  "saturation": -23
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "labels.text.fill",
+          "stylers": [
+              {
+                  "hue": "#007fff"
+              },
+              {
+                  "gamma": 0.77
+              },
+              {
+                  "saturation": 65
+              },
+              {
+                  "lightness": 99
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+              {
+                  "gamma": 0.11
+              },
+              {
+                  "weight": 5.6
+              },
+              {
+                  "saturation": 99
+              },
+              {
+                  "hue": "#0091ff"
+              }
+          ]
+      },
+      {
+          "featureType": "transit.line",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "lightness": -48
+              },
+              {
+                  "hue": "#ff5e00"
+              },
+              {
+                  "gamma": 1.2
+              },
+              {
+                  "saturation": -23
+              }
+          ]
+      },
+      {
+          "featureType": "transit",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+              {
+                  "saturation": -64
+              },
+              {
+                  "hue": "#ff9100"
+              },
+              {
+                  "lightness": 16
+              },
+              {
+                  "gamma": 0.47
+              },
+              {
+                  "weight": 2.7
+              }
+          ]
+      }];
+
+        var mapOptions = {
+            center: { lat: map_lat, lng: map_lng },
+            scrollwheel: false,
+            draggable: false,
+            zoom: 14,
+            mapTypeControl: false,
+            panControl: false,
+            zoomControlOptions: {
+                position: google.maps.ControlPosition.LEFT_CENTER
+            }
+        };
+
+        console.log(map_lat);
+        console.log(map_lng);
+
+        if (desktop) {
+          mapOptions.draggable = true;
+        }
+
+        map = new google.maps.Map(document.getElementById('booking-map'), mapOptions);
+        map.setOptions({styles: styles});
+
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(map_lat, map_lng),
+            icon: {
+                path: google.maps.SymbolPath.CIRCLE,
+                scale: 10
+            },
+            draggable: false,
+            map: map
+        });
+  }
+
+  if ($('#booking-map').length ) {
+    initMap();
+  }
 
   // Sett sticky-block på restaurantblokker
   var restaurant_stickyOptions = {
