3cfa7b5a6bb72086e512668d99d23603b2acb607
KennyBones <raziel014@gmail.com>
2017-10-05 16:27:53 +0200

    Bildetopp

--- db/AUTO_dump_LOCAL_git(master).sql
+++ db/AUTO_dump_LOCAL_git(master).sql
@@ -3,7 +3,7 @@
 -- Host: localhost	Database: festningen_craft
 -- ------------------------------------------------------
 -- Server version 	5.6.28
--- Date: Thu, 05 Oct 2017 14:22:37 +0000
+-- Date: Thu, 05 Oct 2017 14:27:50 +0000
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -3551,4 +3551,4 @@ COMMIT;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on: Thu, 05 Oct 2017 14:22:39 +0000
+-- Dump completed on: Thu, 05 Oct 2017 14:27:51 +0000
